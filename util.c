d361d4bdab2fdda42acd751fa45497befb20ff7d
Kyle Evans <kevans91@ksu.edu>
2017-02-18 00:27:47 -0600

    Rewrite bits of line matching and switch up context printing to pass NetBSD test cases

--- util.c
+++ util.c
@@ -52,8 +52,15 @@ __FBSDID("$FreeBSD$");
 #include "fastmatch.h"
 #include "grep.h"
 
+#ifndef MAX
+#define MAX(a,b)			((a > b) ? (a) : (b))
+#endif
+
 static int	 linesqueued;
 static int	 procline(struct str *l, int);
+
+static int   lasta;
+static bool  ctxover;
 
 bool
 file_matching(const char *fname)
@@ -224,8 +231,10 @@ procfile(const char *fn)
 	strcpy(ln.file, fn);
 	ln.line_no = 0;
 	ln.len = 0;
+	ctxover = false;
 	linesqueued = 0;
 	tail = 0;
+	lasta = 0;
 	ln.off = -1;
 
 	for (c = 0;  c == 0 || !(lflag || qflag); ) {
@@ -236,7 +245,7 @@ procfile(const char *fn)
 			else
 				break;
 		}
-		if (ln.len > 0 && ln.dat[ln.len - 1] == '\n')
+		if (ln.len > 0 && ln.dat[ln.len - 1] == fileeol)
 			--ln.len;
 		ln.line_no++;
 
@@ -247,10 +256,21 @@ procfile(const char *fn)
 			free(f);
 			return (0);
 		}
-		/* Process the file line-by-line */
+
+		/* Process the file line-by-line, enqueue non-matching lines */
 		if ((t = procline(&ln, f->binary)) == 0 && Bflag > 0) {
-			enqueue(&ln);
-			linesqueued++;
+			/* Except don't enqueue lines that appear in -A ctx */
+			if (ln.line_no == 0 || lasta != ln.line_no) {
+				/* queue is maxed to Bflag number of lines */
+				enqueue(&ln);
+				linesqueued++;
+				ctxover = false;
+			} else {
+				/* Indicate to procline() that we have ctx overlap, make sure queue is empty at this point */ 
+				if (!ctxover)
+					clearqueue();
+				ctxover = true;
+			}
 		}
 		c += t;
 		if (mflag && mcount <= 0)
@@ -291,28 +311,29 @@ static int
 procline(struct str *l, int nottext)
 {
 	regmatch_t matches[MAX_LINE_MATCHES];
-	regmatch_t pmatch;
-	size_t st = 0;
+	regmatch_t pmatch, lastmatch;
+	size_t st = 0, nst = 0;
 	unsigned int i;
-	int c = 0, m = 0, r = 0;
+	int c = 0, m = 0, r = 0, lastmatches = 0, leflags = eflags;
+	int startm = 0;
 
 	/* Loop to process the whole line */
 	while (st <= l->len) {
-		pmatch.rm_so = st;
-		pmatch.rm_eo = l->len;
-
+		lastmatches = 0;
+		startm = m;
+		if (st > 0)
+			leflags |= REG_NOTBOL;
 		/* Loop to compare with all the patterns */
 		for (i = 0; i < patterns; i++) {
+			pmatch.rm_so = st;
+			pmatch.rm_eo = l->len;
 			if (fg_pattern[i].pattern)
 				r = fastexec(&fg_pattern[i],
-				    l->dat, 1, &pmatch, eflags);
+				    l->dat, 1, &pmatch, leflags);
 			else
 				r = regexec(&r_pattern[i], l->dat, 1,
-				    &pmatch, eflags);
+				    &pmatch, leflags);
 			r = (r == 0) ? 0 : REG_NOMATCH;
-			st = (cflags & REG_NOSUB)
-				? (size_t)l->len
-				: (size_t)pmatch.rm_eo;
 			if (r == REG_NOMATCH)
 				continue;
 			/* Check for full match */
@@ -339,10 +360,29 @@ procline(struct str *l, int nottext)
 					r = REG_NOMATCH;
 			}
 			if (r == 0) {
+				lastmatches ++;
+				lastmatch = pmatch;
+				/* Skip over zero-length matches */
+				if (pmatch.rm_so == pmatch.rm_eo)
+					continue;
 				if (m == 0)
 					c++;
-				if (m < MAX_LINE_MATCHES)
-					matches[m++] = pmatch;
+
+				if (m < MAX_LINE_MATCHES) {
+					/* Replace previous match if the new one is earlier and/or longer */
+					if (m > startm) {
+						if (pmatch.rm_so < matches[m-1].rm_so ||
+						    (pmatch.rm_so == matches[m-1].rm_so && (pmatch.rm_eo - pmatch.rm_so) > (matches[m-1].rm_eo - matches[m-1].rm_so))) {
+							matches[m-1] = pmatch;
+							nst = pmatch.rm_eo;
+						}
+					} else {
+						/* Advance as normal if not */
+						matches[m++] = pmatch;
+						nst = pmatch.rm_eo;
+					}
+				}
+
 				/* matches - skip further patterns */
 				if ((color == NULL && !oflag) ||
 				    qflag || lflag)
@@ -359,8 +399,19 @@ procline(struct str *l, int nottext)
 		if (!wflag && ((color == NULL && !oflag) || qflag || lflag || Lflag))
 			break;
 
-		if (st == (size_t)pmatch.rm_so)
-			break; 	/* No matches */
+		/* If we didn't have any matches or REG_NOSUB set */
+		if (lastmatches == 0 || (cflags & REG_NOSUB))
+			nst = l->len;
+
+		if (lastmatches == 0)
+			/* No matches */
+			break;
+		else if (st == nst && lastmatch.rm_so == lastmatch.rm_eo)
+			/* Zero-length match -- advance one more so we don't get stuck */
+			nst ++;
+
+		/* Advance st based on previous matches */
+		st = nst;
 	}
 
 
@@ -374,17 +425,22 @@ procline(struct str *l, int nottext)
 	/* Dealing with the context */
 	if ((tail || c) && !cflag && !qflag && !lflag && !Lflag) {
 		if (c) {
-			if (!first && !prev && !tail && Aflag)
+			if (!first && !prev && !tail && (Bflag || Aflag) && !ctxover)
 				printf("--\n");
 			tail = Aflag;
 			if (Bflag > 0) {
-				if (!first && !prev)
-					printf("--\n");
+				/* As well as anything before it */
+//				if (!first && !prev && !ctxover)
+//					printf("--\n");
+				/* Print the queue and clear ctxover if we're printing previous context */
 				printqueue();
+				ctxover = false;
 			}
 			linesqueued = 0;
 			printline(l, ':', matches, m);
 		} else {
+			/* Print -A lines following matches */
+			lasta = l->line_no;
 			printline(l, '-', matches, m);
 			tail--;
 		}
@@ -459,6 +515,10 @@ printline(struct str *line, int sep, regmatch_t *matches, int m)
 	size_t a = 0;
 	int i, n = 0;
 
+	/* If matchall, everything matches but don't actually print for -o */
+	if (oflag && matchall)
+		return;
+
 	if (!hflag) {
 		if (!nullflag) {
 			fputs(line->file, stdout);
@@ -507,6 +567,6 @@ printline(struct str *line, int sep, regmatch_t *matches, int m)
 		}
 	} else {
 		fwrite(line->dat, line->len, 1, stdout);
-		putchar('\n');
+		putchar(fileeol);
 	}
 }
